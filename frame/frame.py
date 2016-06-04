2850a6e3c9a59d35c8d6fc76175301cbe8628fc3
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-04 09:39:46 +0800

    Create a function that accepts a list and returns a 'framed' version of the list

--- frame/frame.py
+++ frame/frame.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
     ===
     frame.py
@@ -14,3 +15,31 @@
     * frame *
     *********
 """
+
+def frame(list):
+    # get longest string in list to determine size of top/bottom of frame
+    longest_string_len = len(max(list, key=len))
+
+    longest_star_len = longest_string_len + 4
+    longest_star_string = '*' * (longest_string_len + 4) + '\n';
+
+    # initialize return string with top of the frame
+    retstr = longest_star_string
+
+    # build out each row of the string
+    for string in list:
+        # calculate number of special characters need for a row not including
+        # the length of the current word in the loop
+        row_spec_len = longest_star_len - len(string)
+
+        rowstr = '* ' + string + ((row_spec_len - 3) * ' ') + '*\n'
+
+        # concatenate row to frame
+        retstr = retstr + rowstr
+
+    # close out return string with bottom of frame
+    retstr = retstr + longest_star_string
+    
+    return retstr
+
+print (frame(["Hello", "World", "in", "a", "frame"]))
