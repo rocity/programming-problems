92714bd8fd970d593700e95c43f472c181ef6a44
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-04 09:13:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming Problems
+
+Attempts to solve programming problems from [Simple Programming Problems](https://adriann.github.io/programming_problems.html)
---
+++ frame/frame.py
@@ -0,0 +1,16 @@
+"""
+    ===
+    frame.py
+    ===
+
+    Write a function that takes a list of strings an prints them, one per line,
+    in a rectangular frame. For example the list
+    ["Hello", "World", "in", "a", "frame"] gets printed as:
+    *********
+    * Hello *
+    * World *
+    * in    *
+    * a     *
+    * frame *
+    *********
+"""
