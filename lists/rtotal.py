1825279c75bbe2e4e81a6ce738c6788a7d5ee093
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-07 09:13:43 +0800

    Create a function that computes the running total of a list

---
+++ lists/rtotal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+    ===
+    rtotal.py
+    ===
+
+    Problem No. 4 in `Lists, Strings`
+    Write a function that computes the running total of a list
+"""
+
+def rtotal(list):
+    ret = []
+    holder = 0
+    for number in list:
+        running = holder + number
+        ret.append(running)
+        holder = running
+    return ret
+
+print (rtotal([1, 2, 3]))
