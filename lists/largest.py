980ebcd2f4b6ab04d7e81cf6ec1119fe93d638b4
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-06-07 08:35:27 +0800

    Create a function that returns the largest element in a list

--- lists/largest.py
+++ lists/largest.py
@@ -3,4 +3,12 @@
     ===
     largest.py
     ===
+
+    Problem No. 1 in `Lists, Strings`
+    Write a function that returns the largest element in a list.
 """
+
+def largest(list):
+    return max(list, key=len)
+
+print (largest(['foo', 'bar', 'spam', 'fubar']))
